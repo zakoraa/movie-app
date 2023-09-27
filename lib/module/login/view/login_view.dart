f1e96d05907b5b72c89db97a571014ada6cbe252
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-27 21:28:34 +0700

    style: update style login page view

--- lib/module/login/view/login_view.dart
+++ lib/module/login/view/login_view.dart
@@ -2,12 +2,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movieapp/module/login/controllers/login_controller.dart';
 import 'package:movieapp/module/signup/view/signup_view.dart';
+import 'package:movieapp/shared/theme/colors.dart';
 import 'package:movieapp/shared/widgets/button_widget.dart';
 import '../../../shared/widgets/form_login_signup.dart';
 import '../../../shared/widgets/textfield_widget.dart';
-import 'package:lottie/lottie.dart';
+// import 'package:lottie/lottie.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -21,67 +23,102 @@ class LoginView extends StatelessWidget {
         content: SizedBox(
       height: Get.size.height,
       width: Get.size.width,
-      child: ListView(physics: const BouncingScrollPhysics(), children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 40),
-          child: SizedBox(
+      child: Center(
+        child: ListView(physics: const BouncingScrollPhysics(), children: [
+          SizedBox(
             width: Get.size.width,
             child: Column(
               children: [
-                Lottie.asset("assets/lottie/login.json",
-                    repeat: true, fit: BoxFit.contain),
-                const Text(
-                  "WELCOME BACK 👋",
-                  style: TextStyle(fontSize: 28),
-                ),
                 const SizedBox(
-                  height: 40.0,
+                  height: 100.0,
                 ),
-                TextFieldWidget(
-                    selectedController: loginController,
-                    controller: loginController.email,
-                    text: "Email",
-                    icon: Icons.email),
-                const SizedBox(
-                  height: 30.0,
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 70),
+                  child: RichText(
+                    text: TextSpan(children: [
+                      TextSpan(
+                        text: "Zako",
+                        style: GoogleFonts.lobster(
+                            fontSize: 70, fontWeight: FontWeight.w100),
+                      ),
+                      TextSpan(
+                        text: "RA",
+                        style: GoogleFonts.lobster(
+                            fontSize: 70,
+                            fontWeight: FontWeight.w100,
+                            color: CustomColor.secondaryBgColor),
+                      ),
+                    ]),
+                  ),
                 ),
-                TextFieldWidget(
-                  selectedController: loginController,
-                  controller: loginController.password,
-                  text: "Password",
-                  icon: Icons.key,
-                  suffixIcon: true,
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 40.0,
-                ),
-                ButtonWidget(
-                    fontSize: 16,
-                    width: Get.size.width * 0.75,
-                    height: 50,
-                    text: "Login",
-                    onTap: () => loginController.loginValidation(context),
-                    controller: loginController),
-                const SizedBox(
-                  height: 15.0,
-                ),
-                GestureDetector(
-                  onTap: () => Get.to(() => const SignupView(),
-                      transition: Transition.fadeIn),
-                  child: const Text(
-                    "Don't have an account?",
-                    style: TextStyle(
-                        fontSize: 11,
-                        fontFamily: "normal",
-                        fontWeight: FontWeight.normal),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      TextFieldWidget(
+                          selectedController: loginController,
+                          controller: loginController.email,
+                          text: "Email",
+                          icon: Icons.email),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      TextFieldWidget(
+                        selectedController: loginController,
+                        controller: loginController.password,
+                        text: "Password",
+                        icon: Icons.key,
+                        suffixIcon: true,
+                        obscureText: true,
+                      ),
+                      const SizedBox(
+                        height: 50.0,
+                      ),
+                      ButtonWidget(
+                          fontSize: 16,
+                          width: Get.width * 0.80,
+                          height: 48,
+                          text: "Login",
+                          onTap: () => loginController.loginValidation(context),
+                          controller: loginController),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't have an account?",
+                            style: TextStyle(
+                                fontSize: 11,
+                                fontFamily: "normal",
+                                fontWeight: FontWeight.normal),
+                          ),
+                          const SizedBox(
+                            width: 3.0,
+                          ),
+                          GestureDetector(
+                            onTap: () => Get.to(() => const SignupView(),
+                                transition: Transition.fadeIn),
+                            child: const Text(
+                              "SignUp",
+                              style: TextStyle(
+                                  fontSize: 11,
+                                  fontFamily: "normal",
+                                  color: CustomColor.yellow,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     ));
   }
 }
