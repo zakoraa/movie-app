60c2961db6221e7b7217c4ddc55ed21ed3fcdd31
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-27 21:28:50 +0700

    style: update style sign up page view

--- lib/module/signup/view/signup_view.dart
+++ lib/module/signup/view/signup_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movieapp/module/signup/controllers/signup_controller.dart';
 import 'package:movieapp/shared/widgets/button_widget.dart';
 import 'package:movieapp/shared/widgets/form_login_signup.dart';
-import 'package:lottie/lottie.dart';
 
+import '../../../shared/theme/colors.dart';
 import '../../../shared/widgets/textfield_widget.dart';
 
 class SignupView extends StatelessWidget {
@@ -24,14 +25,24 @@ class SignupView extends StatelessWidget {
               width: Get.size.width,
               child: Column(
                 children: [
-                  Lottie.asset("assets/lottie/login.json",
-                      repeat: true, fit: BoxFit.contain),
-                  const Text(
-                    "WELCOME 👋",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  const SizedBox(
-                    height: 40.0,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 50),
+                    child: RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                          text: "Zako",
+                          style: GoogleFonts.lobster(
+                              fontSize: 70, fontWeight: FontWeight.w100),
+                        ),
+                        TextSpan(
+                          text: "RA",
+                          style: GoogleFonts.lobster(
+                              fontSize: 70,
+                              fontWeight: FontWeight.w100,
+                              color: CustomColor.secondaryBgColor),
+                        ),
+                      ]),
+                    ),
                   ),
                   TextFieldWidget(
                       selectedController: signupController,
@@ -81,15 +92,31 @@ class SignupView extends StatelessWidget {
                   const SizedBox(
                     height: 15.0,
                   ),
-                  GestureDetector(
-                    onTap: () => Get.back(),
-                    child: const Text(
-                      "Already have an account?",
-                      style: TextStyle(
-                          fontSize: 11,
-                          fontFamily: "normal",
-                          fontWeight: FontWeight.normal),
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                            fontSize: 11,
+                            fontFamily: "normal",
+                            fontWeight: FontWeight.normal),
+                      ),
+                      const SizedBox(
+                        width: 3.0,
+                      ),
+                      GestureDetector(
+                        onTap: () => Get.back(),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                              fontSize: 11,
+                              fontFamily: "normal",
+                              color: CustomColor.yellow,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
